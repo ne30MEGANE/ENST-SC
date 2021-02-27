e47fdd1ee69b7f0d37f3876c65339343120cd25a
Takahashi <ne300229@senshu-u.jp>
2021-02-28 01:25:29 +0900

    楽曲データのAPIを取得できるところまで

--- src/components/Songs.vue
+++ src/components/Songs.vue
@@ -3,3 +3,28 @@
     <h3>songs</h3>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "songs",
+  data() {
+    return {
+      songs: [],
+      loaded: false
+    };
+  },
+  mounted() {
+    axios.get( process.env.VUE_APP_MUSIC_API_URL , {
+      headers: { "X-API-KEY": process.env.VUE_APP_MUSIC_API_KEY },
+      params: { limit: 500 }
+    }).then((response) => {
+      console.log(response)
+      this.songs = response.data.contents;
+      this.loaded = true; // 読み込み終わったらture
+    });
+  }
+}
+
+</script>
