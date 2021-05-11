11658e705eb47b12d380ac997d9be8fdeb8a6a4e
Takahashi <ne300229@senshu-u.jp>
2021-05-11 11:09:03 +0900

    フッター表記・スタイル追加

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,26 @@
 <template>
-  <footer>
-    footer
+  <footer id="footer">
+    <p>
+      enst_sc!! developed by @ed_mgn<br>
+      「あんさんぶるスターズ!! Music」&copy;Happy Elements K.K
+    </p>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+@import "../stylesheets/variables";
+
+#footer{
+  background-color: $light_gray;
+  margin-bottom: 0;
+  padding: 0.5em 0;
+  p{
+    margin: 0;
+    font-size: 14pt;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    color: white;
+    text-align: center;
+  }
+}
+</style>
